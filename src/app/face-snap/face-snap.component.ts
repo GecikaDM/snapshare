d4bfffadbdb8065e8b06b1ea71284c95166eb0c5
GécikaDM <gecika@macbook-pro-de-gecikadm.home>
2023-03-05 18:03:01 +0100

    add datas in component

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit{
+  title! : string;
+  description! : string;
+  createDate! : Date;
+  snaps! : number;
 
+  ngOnInit() {
+    this.title = "Jaycee";
+    this.description = "I am the owner of the application";
+    this.createDate = new Date();
+    this.snaps = 6;
+  }
 }
+
