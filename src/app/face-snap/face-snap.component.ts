15a48008c4ee4ffd40737eb48c256c6b5949502c
GécikaDM <gecika@macbook-pro-de-gecikadm.home>
2023-03-05 19:12:48 +0100

    add-img-style

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -10,12 +10,14 @@ export class FaceSnapComponent implements OnInit{
   description! : string;
   createDate! : Date;
   snaps! : number;
+  imageUrl! : string;
 
   ngOnInit() {
     this.title = "Jaycee";
     this.description = "I am the owner of the application";
     this.createDate = new Date();
     this.snaps = 6;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2016/07/19/04/40/moon-1527501_1280.jpg';
   }
 }
 
